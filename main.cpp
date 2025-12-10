83b94c3c012ea5dbfb06cc10567d01f6c83c71a9
MisterE1234 <MisterE1234@outlook.com>
2025-12-10 12:15:43 -0800

    2nd Commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+//COMSC-210|Ethan Aylard|Final-pt-1
+//IDE Used:Visual Studio
+
+#include <iostream>
+#include <fstream>
+#include <map>
+using namespace std;
+
+const bool debug = true;
+
+int main (){
+    map<string, int> airport;
+    string tempStr;
+    fstream iFile;
+
+    iFile.open("210-final-1-FA25.txt");
+
+    if (!iFile){
+        cout << "File failed to open...\n";
+        return -1;
+    }
+
+    if(debug){
+        cout << "File Opened!\n";
+    }
+    while (iFile.e)
+    iFile >> tempStr;
+
+
+
+    return 1;
+}
