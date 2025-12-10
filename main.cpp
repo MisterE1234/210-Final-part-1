9019d800e23c0186c8821150eb210795e0e91a8e
MisterE1234 <MisterE1234@outlook.com>
2025-12-10 12:38:15 -0800

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -6,13 +6,14 @@
 #include <map>
 using namespace std;
 
-const bool debug = true;
+const bool debug = false;
 
 int main (){
     map<string, int> airport;
     string tempStr;
     ifstream iFile;
 
+    //This code block opens the file and reads the data to the aiport map and displays it.
     iFile.open("210-final-1-FA25.txt");
 
     if (!iFile){
@@ -24,7 +25,7 @@ int main (){
         cout << "File Opened!\n";
     }
 
-    while (iFile.eof()){
+    while (!iFile.eof()){
     iFile >> tempStr;
 
     if(debug){
@@ -44,13 +45,19 @@ int main (){
             cout << "New Airport: " << tempStr << endl;
         }
     }
+    if(debug){
+        cout << "end of the while loop\n";
+    }
 
 
     }
 
+
+    cout << "Airport Visitation Data:\n";
     for(auto data : airport){
-        
+        cout << "Airport Code: " << data.first << ": number of visits: " << data.second << endl;
     }
+    cout << endl;
 
 
 
